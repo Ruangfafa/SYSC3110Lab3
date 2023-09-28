74899383a7614d1e89a8cb43ad5ed63c1552e1b0
ericmao <ericmao@cmail.carleton.ca>
2023-09-28 13:20:57 -0400

    Lab3

---
+++ src/AddressBook.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+public class AddressBook {
+    private ArrayList<BuddyInfo> allBuddyInfo = new ArrayList<>();
+
+    public void addBuddy(BuddyInfo buddyInfo){
+        allBuddyInfo.add(buddyInfo);
+    }
+    public void removeBuddy(BuddyInfo buddyInfo){
+        allBuddyInfo.remove(buddyInfo);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
+    }
+}
+
+
